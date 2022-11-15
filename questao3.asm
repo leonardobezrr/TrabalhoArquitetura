d1d926ca8bc7d33964ce06991019ea11e5bfe290
Gedsonfa <106558706+Gedsonfa@users.noreply.github.com>
2022-11-15 12:27:52 -0300

    questão pronta

--- questao3.asm
+++ questao3.asm
@@ -20,7 +20,16 @@ mainEnd:
 
 power:
 
-	beq $1, $zero, resuUm
+	move $t4, $t0
+	subi $t5, $t1, 1
+
+	beq $t1, $zero, resuUm
+	while:
+		beq $t2, $t5, saida
+		mul $s0, $t0, $t4
+		move $t4, $s0
+		addi $t2, $t2, 1
+		j while
 	j mainEnd
 
 resuUm:
@@ -30,7 +39,12 @@ resuUm:
 	move $a0, $t3
 	syscall
 	j encerraPro
-	
+
+saida:	
+	li $v0, 1
+	move $a0, $t4
+	syscall
+	j encerraPro
 encerraPro:
 	li $v0, 10
 	syscall
